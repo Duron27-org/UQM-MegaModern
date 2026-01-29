e98521e48bc9e8b8c8d71331b85de5aa6b836eac
PragmaNull <79275266+PragmaNull@users.noreply.github.com>
2026-01-29 07:11:01 -0700

    Renamed .c to .cpp to make compiling as C++ easier.

--- src/libs/math/random.h
+++ src/libs/math/random.h
@@ -61,7 +61,7 @@ struct RandomContext {
 #define PrimeSeed (optSeedType == OPTVAL_PRIME)
 #define StarSeed (optSeedType > OPTVAL_PLANET)
 
-static inline UNICODE *
+static inline const UNICODE *
 SeedStr (void)
 {
 	switch (optSeedType)
